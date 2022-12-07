c1166ad5b1bc345c4fa952b24d5fd808a6dc7af0
John Climie <johnclimie00@gmail.com>
2022-12-07 12:01:21 -0500

    Troubleshooting image

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css'
+import './App.css';
+
 
 function App() {
   return (
     <div className="main-container">
-
+      <div className="login-container">
+          <div className="img-container">
+            
+          </div>
+      </div>
     </div>
   )
 }
