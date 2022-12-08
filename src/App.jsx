71a5323e9b049e453b9850dbfe60d7b56e7a0a10
John Climie <johnclimie00@gmail.com>
2022-12-08 10:43:53 -0500

    Added HTML and SVG

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import mtnImg from './images/test.jpg';
 
 function App() {
   return (
-    <h1>test</h1>
+    <div className="main-container">
+      <div className="login-container">
+        <div className="header-container">
+          <h1>Login</h1>
+        </div>
+
+        <div className="user-container">
+          <div className="input-container">
+            <label for='username'>Username</label>
+            <input type='text' className='input' id='username'></input>
+          </div>
+          <div className="input-container">
+            <label for='password'>Password</label>
+            <input type='password' className='input' id='password'></input>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
